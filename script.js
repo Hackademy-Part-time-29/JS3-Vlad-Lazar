2c1b32c18f6d67e652f6ba6c73b868e49eb079e1
drakdy <157641567+drakdy@users.noreply.github.com>
2024-02-22 19:36:36 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+//Traccia 1
+// let n=2;
+// for(let i=1; i<=10; i++){
+//     console.log(n*i);
+// }
+
+//Traccia 2
+// let somma=0;
+// let c=0;
+// for(let i=1; i<=20; i++){
+//     if(i%2==0){
+//         console.log(i);
+//     }else{
+//         somma=somma+i;
+//         c++;
+//     }
+// }
+// console.log(`La media è: ${somma/c}`);
+
+//Traccia 3
+// let n=parseInt(prompt("Inserisci il numero della bevanda"));
+// let t=1;
+// for(let i=0; i<=t; i++){
+//     if(n!=1 && n!=2 && n!=3){
+//         t++;
+//         n=parseInt(prompt("Reinserisci il numero della bevanda"));
+//     }else if(n==1){
+//         console.log("È stata selezionata l'acqua")
+//     }else if(n==2){
+//         console.log("È stata selezionata la Coca Cola");
+//     }else if(n==3){
+//         console.log("È stata selezionata la birra");
+//     }
+// }
